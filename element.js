fd282e26cc6f18a34292dcbbc4a15e016b117b8f
Danny Engelman <danny@engelman.nl>
2023-04-15 14:41:04 +0200

    updated documentation

--- element.js
+++ element.js
@@ -1,40 +1,39 @@
 
 customElements.define("date-counter", class extends HTMLElement {
     get date() {
+        // get date from attribute or default Y2K38 Epochalypse date
         return new Date(this.getAttribute("date") || "2038-01-19 03:14:07");
     }
     get eventname() {
+        // get event name from attribute or default Y2K38 Epochalypse name
         return this.getAttribute("event") || "Y2K38 Epochalypse <a href='https://en.wikipedia.org/wiki/Year_2038_problem' style='text-decoration:none;font-size:40%'>wtf?</a>";
     }
-    get count() {
-        return ["years", "days", "hours", "minutes", "seconds"];
-    }
     connectedCallback() {
+        let count = ["years", "days", "hours", "minutes", "seconds"];
         // set counting years,days,hours,minutes,seconds
-        var countlabels =
-            // user defined:
+        let countlabels =
+            // user defined "years,days"string from attribute "count" 
             this.getAttribute("count")?.split(",")
-            // or use default this.count, and exclude all "noyears" "noseconds" attributes
-            || this.count.filter(label => !this.hasAttribute("no" + label));
+            //or default "years,days,hours,minutes,seconds":
+            || count.filter(label => !this.hasAttribute("no" + label)); // filter away user defined "noXXXX" attributes
         // init labels to be shown
-        var labels =
+        let labels =
             // user defined:
-            this.count.map(
+            count.map(
                 label => (new Intl.RelativeTimeFormat((this.getAttribute("locale") || "en"), { numeric: 'auto' }))
                     .formatToParts(10, label)[2].value.trim()
             )
-        //.filter(label=>this.count.includes(label));
+        //.filter(label=>count.includes(label));
 
         // shorten the default years,days,hours,minutes,seconds to the number of user-defined labels
         //countlabels = countlabels.slice(-1 * (labels.length));
-        var localelabels = this.count.reduce((acc, label, idx) => {
+        let localelabels = count.reduce((acc, label, idx) => {
             if (countlabels.includes(label)) acc.push(labels[idx]);
             return acc;
         }, []);
-        console.error(labels, countlabels, localelabels);
 
         // generic function to create a HTML element with all content and properties
-        var element = ({ tag = "div", id, append = [], ...props }) => {
+        let element = ({ tag = "div", id, append = [], ...props }) => {
             // every id must be unique! and becomes a this. reference
             this[id] = Object.assign(document.createElement(tag), { id, ...props });
             this[id].append(...append);
@@ -43,18 +42,18 @@ customElements.define("date-counter", class extends HTMLElement {
         }
         // generic function setting CSS selector
         // to read value from attribues OR CSS property OR default value
-        var attr_CSSprop = (prefix, name, value, // parameters
+        let attr_CSSprop = (prefix, name, value, // parameters
             // abusing parameters as variable declarations to avoid needing a function body and 'return' statement
             attr = `${prefix}-${name}`, // eg. event-background
             cssprop = `--${this.localName}-${attr}`, // eg. --date-counter-event-background
             //            l = console.log(attr, this.getAttribute(attr))
         ) => `${name}:${this.getAttribute(attr) || `var(${cssprop},${value})`};`;
 
-        // getComputedStyle(this).getPropertyValue(attr).replace(/"/g, "").trim()
-
+        // create full shadowDOM
         this.attachShadow({ mode: "open" }).append(
             element({
                 tag: "style",
+                id: "date-counter-style",
                 innerHTML: `:host {display:inline-block;` +
                     attr_CSSprop("", "font-family", "arial") +
                     attr_CSSprop("", "width", "", countlabels.length * 6 + "rem") +
@@ -89,29 +88,34 @@ customElements.define("date-counter", class extends HTMLElement {
                             innerHTML: (localelabels[countlabels.indexOf(id)] || "").toUpperCase()
                         })]
                 }))
-            }))
-        var timer = setInterval(() => {
-            var diff = this.difference(this.date);
+            }))// shadowDOM created
+
+        // main interval timer
+        let timer = setInterval(() => {
+            let diff = this.difference(this.date);
             countlabels.map(label => (this["_" + label] == diff[label])
                 ? 0
-                : this[label].innerHTML = (this["_" + label] = diff[label]));
+                : this[label].innerHTML = (this["_" + label] = diff[label])); // update counter
             if (countlabels.every(label => diff[label] == 0)) clearInterval(timer);
         }, 1e3);
 
     } // connectedCallback
+
+    // keeping as separate methode for easy reuse in other projects
+    // could be included in this connectedCallback for a smaller file
     difference(date, start = new Date(), future = new Date(date)) {
-        var since = future < start && ([start, future] = [future, start]);
-        var diff = future - start;
-        var day = 1000 * 60 * 60 * 24;
-        var timediff = { years: ~~(diff / (day * 365)) }
-        var leapYears = 0;
-        for (var i = start.getFullYear(); i < future.getFullYear(); i++)
+        let since = future < start && ([start, future] = [future, start]);
+        let diff = future - start;
+        let day = 1e3 * 60 * 60 * 24;
+        let timediff = { years: ~~(diff / (day * 365)) }
+        let leapYears = 0;
+        for (let i = start.getFullYear(); i < future.getFullYear(); i++)
             ((i % 4 === 0 && i % 100 !== 0) || i % 400 === 0) && (since ? leapYears-- : leapYears++);
         //timediff.weeks = ~~((diff -= timediff.years * day * 7)/day);
         timediff.days = ~~((diff -= timediff.years * day * 365) / day) + leapYears;
-        timediff.hours = ~~((diff -= (timediff.days - leapYears) * day) / (1000 * 60 * 60));
-        timediff.minutes = ~~((diff -= timediff.hours * 1000 * 60 * 60) / (1000 * 60));
-        timediff.seconds = ~~((diff -= timediff.minutes * 1000 * 60) / 1000);
+        timediff.hours = ~~((diff -= (timediff.days - leapYears) * day) / (1e3 * 60 * 60));
+        timediff.minutes = ~~((diff -= timediff.hours * 1e3 * 60 * 60) / (1e3 * 60));
+        timediff.seconds = ~~((diff -= timediff.minutes * 1e3 * 60) / 1e3);
         return timediff;
     }
 })
