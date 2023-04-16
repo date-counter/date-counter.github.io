c816bc40d61bb31e3474bc0ea5731f87c82c299a
Danny Engelman <danny@engelman.nl>
2023-04-16 16:15:19 +0200

    re minify

--- element.js
+++ element.js
@@ -9,51 +9,51 @@ customElements.define("date-counter", class extends HTMLElement {
         return this.getAttribute("event") || "Y2K38 Epochalypse <a href=//en.wikipedia.org/wiki/Y2K38 style=font-size:40%>wtf?</a>";
     }
     connectedCallback(
+    ) {
         // naming all my variables VAR, they are slight faster and minify well because CSS has a "var" keyword too
-        count = ["years", "days", "hours", "minutes", "seconds"],
+        var count = ["years", "days", "hours", "minutes", "seconds"];
 
         // set counting years,days,hours,minutes,seconds
-        countlabels =
+        var countlabels =
             // user defined "years,days"string from attribute "count" 
             this.getAttribute("count")?.split(",")
             // or default "years,days,hours,minutes,seconds":
             // filter away user defined "noXXXX" attributes
-            || count.filter(label => !this.hasAttribute("no" + label)),
+            || count.filter(label => !this.hasAttribute("no" + label));
 
         // init labels to be shown
-        locale_labels =
+        var locale_labels =
             // get proper langauge names for all counting labels
             count.map(
                 label => (new Intl.RelativeTimeFormat((this.getAttribute("locale") || "en"), { numeric: "auto" }))
                     .formatToParts(10, label)[2].value.trim()
-            ),
+            );
 
         // generic function to create a HTML element with all content and properties
-        element = ({ tag = "div", id, append = [], ...props }) => {
+        var element = ({ tag = "div", id, append = [], ...props }) => {
             // every id must be unique! and becomes a this. reference
             this[id] = Object.assign(document.createElement(tag), { id, ...props });
             this[id].append(...append);
             return this[this[id].part = id]; // merged 2 JS lines into one for shorter code
-        },
+        };
         // generic function setting CSS selector
         // to read value from attribues OR CSS property OR default value
-        attr_CSSprop = (prefix, name, value, // parameters
+        var attr_CSSprop = (prefix, name, value, // parameters
             // abusing parameters as variable declarations to avoid needing a function body and "return" statement
             attr = `${prefix}-${name}`, // eg. event-background
             cssprop = `--${this.localName}-${attr}`, // eg. --date-counter-event-background
             //            l = console.log(attr, this.getAttribute(attr))
-        ) => `${name}:${this.getAttribute(attr) || `var(${cssprop},${value})`};`,
+        ) => `${name}:${this.getAttribute(attr) || `var(${cssprop},${value})`};`;
 
         // declare here to prevent needing let inside code below
-        timer, datedifference
+        var timer, datedifference;
 
-    ) {
         // create full shadowDOM
         this.attachShadow({ mode: "open" }).append(
             element({
                 tag: "style",
                 //id: "style", // prevent from setting default "undefined" string value
-                innerHTML: `:host {display:inline-block;` +
+                innerHTML: `:host{display:inline-block;` +
                     attr_CSSprop("", "font-family", "arial") +
                     attr_CSSprop("", "width", "", countlabels.length * 5 + "rem") +
                     `}` +
@@ -114,22 +114,19 @@ customElements.define("date-counter", class extends HTMLElement {
 
     // keeping as separate methode for easy reuse in other projects
     // could be included in this connectedCallback for a smaller file
-    difference(date, start = new Date(), future = new Date(date),
-        // local variables, declared here to prevent let
-        since = future < start && ([start, future] = [future, start]),
-        diff = future - start,
-        day = 1e3 * 60 * 60 * 24,
-        timediff = { years: ~~(diff / (day * 365)) },
-        leapYears = 0,
-        i // for loop variable, saves using let
-    ) {
-        for (i = start.getFullYear(); i < future.getFullYear(); i++)
+    difference(date, start = new Date(), future = new Date(date)) {
+        var since = future < start && ([start, future] = [future, start]);
+        var diff = future - start;
+        var day = 864e5;
+        var timediff = { years: ~~(diff / (day * 365)) };
+        var leapYears = 0;
+        for (var i = start.getFullYear(); i < future.getFullYear(); i++)
             ((i % 4 == 0 && i % 100 != 0) || i % 400 == 0) && (since ? leapYears-- : leapYears++);
         //timediff.weeks = ~~((diff -= timediff.years * day * 7)/day);
         timediff.days = ~~((diff -= timediff.years * day * 365) / day) + leapYears;
-        timediff.hours = ~~((diff -= (timediff.days - leapYears) * day) / (1e3 * 60 * 60));
-        timediff.minutes = ~~((diff -= timediff.hours * 1e3 * 60 * 60) / (1e3 * 60));
-        timediff.seconds = ~~((diff -= timediff.minutes * 1e3 * 60) / 1e3);
+        timediff.hours = ~~((diff -= (timediff.days - leapYears) * day) / 36e5);
+        timediff.minutes = ~~((diff -= timediff.hours * 36e5) / (6e4));
+        timediff.seconds = ~~((diff -= timediff.minutes * 6e4) / 1e3);
         return timediff;
     }
 })
